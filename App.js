4a117b480a76d0fb67a13c2cf38a69f36ffaba15
Kelly Cruz <kellycruz.ti@gmail.com>
2023-04-23 14:35:40 -0300

    Add Header

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello  World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Header />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello  World!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
