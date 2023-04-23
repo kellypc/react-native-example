20eff04f577d4ab91b348e744ad5a56ef82cf658
Kelly Cruz <kellycruz.ti@gmail.com>
2023-04-23 15:09:54 -0300

    Add parameterization  in Header and create Footer

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <>
-      <Header />
+      <Header title="Header Param" />
       <View style={styles.container}>
         <Text style={styles.title}>Hello  World!</Text>
         <StatusBar style="auto" />
       </View>
+      <Footer title="Footer Param" />
     </>
   );
 }
